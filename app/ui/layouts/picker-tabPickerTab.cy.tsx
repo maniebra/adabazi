d73205dcc627b5e4495066fae7a59c5c95695494
maniebra <mani.ebra@gmail.com>
2025-12-19 18:16:41 +0330

    remove cypress problematic agc

--- app/ui/layouts/picker-tabPickerTab.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import PickerTab from './picker-tab'
-
-describe('<PickerTab />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<PickerTab />)
-  })
-})
